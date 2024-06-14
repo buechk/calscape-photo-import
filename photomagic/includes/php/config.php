28b1f1899e39b5f3baa9389b7de28b19b97f7f0a
Kristy Bueche <kristyabueche@gmail.com>
2024-06-13 19:49:02 -0700

    Update config.php

--- photomagic/includes/php/config.php
+++ photomagic/includes/php/config.php
@@ -23,50 +23,14 @@ define('COLLECTION_PHOTOS_DIR', COLLECTIONS_ROOT . 'photos/');
 define('COLLECTION_THUMBNAILS_DIR', COLLECTIONS_ROOT . 'thumbnails/');
 define('COLLECTIONS_EXPORT_DIR', COLLECTIONS_ROOT . 'exports/');
 
-/*
+
 // Kristy's calsca6_calscape
 $dbConfig = array(
     'hostname' => "localhost:3306",
     'username' => "root",
-    'password' => "Edw8rdpm!",
+    'password' => "",
     'database' => "calsca6_calscape"
 );
-*/
 
-/*
-// AWS Server Database connection parameters
-$dbConfig = array(
-    'hostname' => "calscape-photo-test-db-instance.cjoscaq8s13i.us-west-1.rds.amazonaws.com:3306",
-    'username' => "photo_admin",
-    'password' => "calscapephoto",
-    'database' => "calsca6_demo"
-);
-*/
 
-/*
-// Calscape 2 UAT
-$dbConfig = array(
-    'hostname' => "calscapeproduction.cdwkpn2rrvdl.us-west-1.rds.amazonaws.com",
-    'username' => "dbase_view",
-    'password' => "ST|v7=g1-PD?3A9!ssp)6Zgt",
-    'database' => "calscape"
-);
-*/
 
-/*
-// Photo Test Calscape 2
-$dbConfig = array(
-    'hostname' => "calscape2-database-1.cjoscaq8s13i.us-west-1.rds.amazonaws.com",
-    'username' => "photo_admin",
-    'password' => "calscapephoto",
-    'database' => "calscape"
-);
-*/
-
-// Photo Test Calscape 2 SSH Tunnel through EC2
-$dbConfig = array(
-    'hostname' => "127.0.0.1:3336",
-    'username' => "photo_admin",
-    'password' => "calscapephoto",
-    'database' => "calscape"
-);
